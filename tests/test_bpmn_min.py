907e4fbc74999c6eb9b219583c1e926a8b4c6aea
Marshall <mbomoore@gmail.com>
2025-08-21 11:13:17 -0400

    Update test imports to use new package structure
    - Fixed merge conflicts in test_bpmn_min.py
    - Updated imports to use agentic_process_automation package structure
    - Preparation for old structure removal

--- tests/test_bpmn_min.py
+++ tests/test_bpmn_min.py
@@ -2,8 +2,8 @@ import sys
 from pathlib import Path
 
 import pytest
-from core.adapters.bpmn_min import from_bpmn_xml
-from core.pir import validate
+from agentic_process_automation.core.adapters.bpmn_min import from_bpmn_xml
+from agentic_process_automation.core.pir import validate
 import types
 
 FIXTURE_SIMPLE = b"""
@@ -57,11 +57,11 @@ def test_dispatcher_prefers_spiff_when_available(monkeypatch):
     sys.modules["spiffworkflow.bpmn"] = mod_bpmn
     sys.modules["spiffworkflow.bpmn.parser"] = mod_parser
 
-    from core.adapters.bpmn import parse_bpmn
+    from agentic_process_automation.core.adapters.bpmn import parse_bpmn
 
     pir = parse_bpmn(FIXTURE_SIMPLE)
     # When Spiff is present, dispatcher should still return a PIR (even if empty via dummy)
-    from core.pir import PIR
+    from agentic_process_automation.core.pir import PIR
     assert isinstance(pir, PIR)
 
 
@@ -73,9 +73,9 @@ def test_dispatcher_falls_back_without_spiff(monkeypatch):
         "spiffworkflow.bpmn.parser",
     ]:
         sys.modules.pop(k, None)
-    from core.adapters.bpmn import parse_bpmn
+    from agentic_process_automation.core.adapters.bpmn import parse_bpmn
     pir = parse_bpmn(FIXTURE_SIMPLE)
-    from core.pir import PIR
+    from agentic_process_automation.core.pir import PIR
     assert isinstance(pir, PIR)
     # Should have parsed nodes/edges via minimal parser
     assert len(pir.nodes) >= 4
@@ -83,8 +83,8 @@ def test_dispatcher_falls_back_without_spiff(monkeypatch):
 
 
 def test_user_task_and_variants_parse_as_task():
-    from core.adapters.bpmn_min import from_bpmn_xml
-    from core.pir import validate
+    from agentic_process_automation.core.adapters.bpmn_min import from_bpmn_xml
+    from agentic_process_automation.core.pir import validate
 
     xml = b"""
     <definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" id=\"Defs_UserTask\"> 
@@ -106,74 +106,5 @@ def test_user_task_and_variants_parse_as_task():
     assert "task" in kinds
     # ensure specific ids were created
     assert {"u1", "u2"}.issubset(set(pir.nodes.keys()))
-<<<<<<< HEAD
-=======
-    def test_dispatcher_prefers_spiff_when_available(monkeypatch):
-        # Simulate SpiffWorkflow being available by creating fake modules
-        import types
-
-        class DummyParser:
-            def parse_xml_string(self, s):
-                class Dummy:
-                    task_specs = {}
-                    sequence_flows = []
-                return Dummy()
-
-        mod_spiff = types.ModuleType("spiffworkflow")
-        mod_bpmn = types.ModuleType("spiffworkflow.bpmn")
-        mod_parser = types.ModuleType("spiffworkflow.bpmn.parser")
-        setattr(mod_parser, "BpmnParser", DummyParser)
-        sys.modules["spiffworkflow"] = mod_spiff
-        sys.modules["spiffworkflow.bpmn"] = mod_bpmn
-        sys.modules["spiffworkflow.bpmn.parser"] = mod_parser
-
-        from core.adapters.bpmn import parse_bpmn
-
-        pir = parse_bpmn(FIXTURE_SIMPLE)
-        # When Spiff is present, dispatcher should still return a PIR (even if empty via dummy)
-        from core.pir import PIR
-        assert isinstance(pir, PIR)
 
 
-    def test_dispatcher_falls_back_without_spiff(monkeypatch):
-        # Ensure spiffworkflow is not present
-        for k in [
-            "spiffworkflow",
-            "spiffworkflow.bpmn",
-            "spiffworkflow.bpmn.parser",
-        ]:
-            sys.modules.pop(k, None)
-        from core.adapters.bpmn import parse_bpmn
-        pir = parse_bpmn(FIXTURE_SIMPLE)
-        from core.pir import PIR
-        assert isinstance(pir, PIR)
-        # Should have parsed nodes/edges via minimal parser
-        assert len(pir.nodes) >= 4
-        assert len(pir.edges) == 4
-
-
-    def test_user_task_and_variants_parse_as_task():
-        from core.adapters.bpmn_min import from_bpmn_xml
-        from core.pir import validate
-
-        xml = b"""
-        <definitions xmlns=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" id=\"Defs_UserTask\"> 
-            <process id=\"P\" isExecutable=\"false\"> 
-                <startEvent id=\"s\"/> 
-                <userTask id=\"u1\" name=\"Do it\"/> 
-                <serviceTask id=\"u2\" name=\"Service\"/> 
-                <endEvent id=\"e\"/> 
-                <sequenceFlow id=\"f1\" sourceRef=\"s\" targetRef=\"u1\"/> 
-                <sequenceFlow id=\"f2\" sourceRef=\"u1\" targetRef=\"u2\"/> 
-                <sequenceFlow id=\"f3\" sourceRef=\"u2\" targetRef=\"e\"/> 
-            </process> 
-        </definitions>
-        """
-        pir = from_bpmn_xml(xml)
-        report = validate(pir)
-        assert report["errors"] == []
-        kinds = {n.kind for n in pir.nodes.values()}
-        assert "task" in kinds
-        # ensure specific ids were created
-        assert {"u1", "u2"}.issubset(set(pir.nodes.keys()))
->>>>>>> origin/main
